456a3e062aa120777f3b64e210d543ffa507b62c
brandpark <pak2005@hanmail.net>
2021-05-20 23:58:40 +0900

    카카오 2021 블라인드 채용 순위검색 문제

---
+++ src/kakao/blind_2021/RankSearch.java
@@ -0,0 +1,13 @@
+package kakao.blind_2021;
+
+/*
+ * 카카오 2021 블라인드 채용 _ 순위검색 문제
+ * 난이도 : 2
+ * link : https://programmers.co.kr/learn/courses/30/lessons/72412
+ * */
+public class RankSearch {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
