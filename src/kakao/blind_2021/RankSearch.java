69b37612d9d0956eeb058ccf0ee968df3fae6634
brandpark <pak2005@hanmail.net>
2021-05-22 19:26:51 +0900

    카카오 2021 블라인드 채용 순위검색 문제 수정

--- src/kakao/blind_2021/RankSearch.java
+++ src/kakao/blind_2021/RankSearch.java
@@ -22,56 +22,84 @@ public class RankSearch {
     //1<= query.length <= 100,000
     //query[i] = "개발언어 and 직군 and 경력 and 소울푸드 최소점수", 와일드카드(-) 가능
 
-    private static HashMap<String, List<Integer>> infoMap = new HashMap<>();
     private static String[] langs = {"cpp", "java", "python", "-"};
     private static String[] poss = {"backend", "frontend", "-"};
     private static String[] careers = {"junior", "senior", "-"};
     private static String[] foods = {"chicken", "pizza", "-"};
+    private static HashMap<String, List<Integer>> lookupMap = new HashMap<>();
 
     public int[] solution(String[] info, String[] query) {
-        int[] answer = new int[query.length];
+        //가능한 조합을 모두 lookupMap에 담는다.
+        for (String l : langs)
+            for (String p : poss)
+                for (String c : careers)
+                    for (String f : foods)
+                        lookupMap.put(l + p + c + f, new ArrayList<>());
 
-        //모든 조합의 경우를 infoMap에 담는다.
-        for(String l : langs)
-            for(String p : poss)
-                for(String c : careers)
-                    for(String f : foods)
-                        infoMap.put(l+p+c+f, new ArrayList<>());
 
         //for in in info
-        for(String in : info){
-            in = in.replaceAll("(\\w+) (\\w+) (\\w+) (\\w+)", "$1$2$3$4");
+        for (String in : info) {
+            //와일드 카드(-)를 사용한 in의 가능한 모든 조합을 map에서 꺼내 점수 리스트에 점수를 넣는다.
             String[] inArr = in.split(" ");
-            //map에서 해당하는 조건을 찾아 점수를 넣는다.
-            infoMap.get(inArr[0]).add(Integer.parseInt(inArr[1]));
+            boolean[] visited = new boolean[4];
+            int score = Integer.parseInt(inArr[4]);
+
+            for (int wild = 0; wild <= 4; wild++) {
+                putInfo(inArr, visited, 0, wild, 0, score);
+            }
         }
 
+        int[] answer = new int[query.length];
         //for q in query
-        for(int i=0;i<query.length;i++){
+        for (int i = 0; i < query.length; i++) {
+            //조건을 키로infoMap에서 점수리스트를 가져온다.
             String q = query[i].replaceAll(" and ", "");
             String[] qArr = q.split(" ");
-            //infoMap에서 조건에 해당하는 학생들의 점수 리스트를 가져와서 오름차순 정렬한다.
-            List<Integer> scoreList = infoMap.get(qArr[0]);
-            Collections.sort(scoreList);
-
-            //이진탐색으로 요구하는 점수 이상의 학생 수를 센다.
-            int index = Collections.binarySearch(scoreList, Integer.parseInt(qArr[1]));
-
-            index = index < 0 ? Math.abs(index + 1) : index;
-
-            int minValue = scoreList.get(index--);
-            while(index >= 0){
-                if(scoreList.get(index) < minValue)
-                    break;
-                minValue = scoreList.get(index);
-                index--;
+            List<Integer> scoreList = lookupMap.get(qArr[0]);
+            //점수리스트가 비어있다면 0을 넣고 다음 q로 넘어간다.
+            if (scoreList.isEmpty()) {
+                answer[i] = 0;
+                continue;
             }
-
-            int result = scoreList.size() - index;
-
-            answer[i] = result;
+            //비어있지 않다면 점수리스트를 정렬하고 이진탐색으로 조건의 점수이상의 학생 수를 알아낸다.
+            int score = Integer.parseInt(qArr[1]);
+            Collections.sort(scoreList);
+            int index = Collections.binarySearch(scoreList, score);
+            if (index < 0) {
+                index = Math.abs(index + 1);
+            } else {
+                for (int k = index - 1; k >= 0; k--) {
+                    if (scoreList.get(k) < score)
+                        break;
+                    index = k;
+                }
+            }
+            answer[i] = scoreList.size() - index;
         }
+
 
         return answer;
     }
+
+    private void putInfo(String[] infos, boolean[] visited, int count, int wild, int index, int score) {
+        if (count == wild) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < visited.length; i++) {
+                if (visited[i])
+                    sb.append("-");
+                else
+                    sb.append(infos[i]);
+            }
+            lookupMap.get(sb.toString()).add(score);
+            return;
+        }
+        for (int i = index; i < visited.length; i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            putInfo(infos, visited, count + 1, wild, i + 1, score);
+            visited[i] = false;
+        }
+    }
+
 }
