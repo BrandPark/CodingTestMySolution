be02929ff276e213735c7cf102dacd457c6700b0
brandpark <pak2005@hanmail.net>
2021-05-27 23:56:30 +0900

    카카오 2021 블라인드 채용 광고삽입 문제

---
+++ src/kakao/blind_2021/AdInsertion.java
@@ -0,0 +1,21 @@
+package kakao.blind_2021;
+import java.util.Arrays;
+
+/*
+ * 카카오 2021 블라인드 채용 _ 광고 삽입문제
+ * 알고리즘 : 단순 구현
+ * 시간복잡도 :
+ * 난이도 : 3
+ * link : https://programmers.co.kr/learn/courses/30/lessons/72414
+ * */
+
+public class AdInsertion {
+    // 사람들이 가장 많이 보는 구간에 광고 삽입
+    // 재생시간 = 끝나는 시간 - 시작시간
+    // play_time, adv_time = HH:MM:SS,
+    // 0 <= HH <= 99, 0 <= MM <= 59, 0 <= MM <= 59
+    // 1<= logs.length <= 300,000
+    public String solution(String play_time, String adv_time, String[] logs) {
+        return "";
+    }
+}
