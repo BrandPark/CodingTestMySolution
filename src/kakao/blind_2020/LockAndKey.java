279a799b4cc4d7dfc1e15e1b4bde3b9de0a679cb
brandpark <pak2005@hanmail.net>
2021-06-06 21:33:18 +0900

    카카오 2020 블라인드 채용 자물쇠와 열쇠(풀이 중)

---
+++ src/kakao/blind_2020/LockAndKey.java
@@ -0,0 +1,13 @@
+package kakao.blind_2020;
+
+public class LockAndKey {
+    public boolean solution(int[][] key, int[][] lock) {
+        // 자물쇠 N * N, 키 M * M
+        // 3<= M <= N <= 20
+        // key는 회전가능하고 lock의 홈을 다 채울 수 있다면 열 수 있다고 판단한다.
+        // 홈 : 0, 돌기 : 1;
+
+        boolean answer = true;
+        return answer;
+    }
+}
