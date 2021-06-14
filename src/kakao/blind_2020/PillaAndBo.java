86e9ecdbb8ef0bf9eacd49aaa6fd845f6cc249f0
brandpark <pak2005@hanmail.net>
2021-06-14 23:27:32 +0900

    카카오 2020 블라인드 채용 기둥과 보(작업3)

--- src/kakao/blind_2020/PillaAndBo.java
+++ src/kakao/blind_2020/PillaAndBo.java
@@ -18,7 +18,7 @@ public class PillaAndBo {
     // b : 0은 삭제, 1은 설치
     // 구조물의 삭제 또는 설치는 (y,x)에서 보는 오른쪽, 기둥은 위쪽 방향으로 한다.
     // 바닥에 보를 설치하는 경우는 없다.
-    // 없는 구조물을 삭제하는 경우는 없다.
+    // 겹치도록 설치하는 경우와, 없는 구조물을 삭제하는 경우는 없다.
     // 기둥은 바닥 위에 있거나 보의 한쪽 끝 부분 위에 있거나, 다른 기둥 위에 있어야 한다.
     // 보는 한쪽 끝 부분이 기둥 위에 있거나, 양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 한다.
     // 작업을 수행한 결과가 조건을 만족하지 않으면 작업은 무시된다.
@@ -38,28 +38,38 @@ public class PillaAndBo {
 
             // 설치
             if(how == INST) {
-                if(isSure(y, x, what, map))
-                    map[y][x] = what;
+                if(isSure(y, x, what, map)){
+                    if(map[y][x] > -1){
+                        map[y][x] |= what;
+                    }else{
+                        map[y][x] = what;
+                    }
+                }
             }
             // 삭제
             else {
                 // 기둥
                 if(what == PILLAR) {
                     // 제거하고
-                    map[y][x] = -1;
-                    // y+1, x에 있는 것이 괜찮은지 확인한다.
-                    // 괜찮지 않다면 다시 돌려 놓는다.
-                    if(!isSure(y+1, x, what, map))
-                        map[y][x] = what;
+                    if(map[y][x] > -1) {
+                        map[y][x] ^= what+1;
+                        // y+1, x에 있는 것이 괜찮은지 확인한다.
+                        // 괜찮지 않다면 다시 돌려 놓는다.
+                        if(!isSure(y+1, x, what, map)){
+                            map[y][x] |= what+1;
+                        }
+                    }
                 }
                 // 보
                 else {
                     // 제거하고
-                    map[y][x] = -1;
-                    // y, x-1과 y, x+1에 있는 것이 모두 괜찮은지 확인한다.
-                    //괜찮지 않다면 다시 돌려 놓는다.
-                    if(!isSure(y, x-1, what, map) || !isSure(y, x+1, what, map))
-                        map[y][x] = what;
+                    if(map[y][x] > -1){
+                        map[y][x] ^= what+1;
+                        // y, x-1과 y, x+1에 있는 것이 모두 괜찮은지 확인한다.
+                        //괜찮지 않다면 다시 돌려 놓는다.
+                        if(!isSure(y, x-1, what, map) || !isSure(y, x+1, what, map))
+                            map[y][x] |= what+1;
+                    }
                 }
             }
         }
@@ -91,21 +101,28 @@ public class PillaAndBo {
     }
     private boolean isSure(int y, int x, int what, int[][] map){
         int n = map.length - 1;
-        if(what == BO){
+        if(what == BO+1){
             // y-1, x 또는 y-1, x+1에 기둥이 있거나, y, x-1과 y,x+1에 보가 있어야한다.
-            if(((!outOfBound(y-1, x, n) && map[y-1][x] == PILLAR)
-                    || (!outOfBound(y-1,x+1,n) && map[y-1][x+1] == PILLAR))
-                    || ((!outOfBound(y,x-1,n) && map[y][x-1] == BO)
-                    && (!outOfBound(y,x+1,n) && map[y][x+1] == BO)))
+            if(((!outOfBound(y-1, x, n) && isPillar(y-1,x,map))
+                    || (!outOfBound(y-1,x+1,n) && isPillar(y-1,x+1,map)))
+                    || ((!outOfBound(y,x-1,n) && isBo(y,x-1,map))
+                    && (!outOfBound(y,x+1,n) && isBo(y,x+1,map))))
                 return true;
             return false;
         }
         else {
             // y-1,x에 기둥이 또는 바닥, 또는 y, x-1에 보가 있어야한다.
-            if(y == 0 || map[y-1][x] == PILLAR || (!outOfBound(y, x-1, n) && map[y][x-1] == BO))
+            if(y == 0 || isPillar(y-1,x,map) || (!outOfBound(y, x-1, n) && isBo(y,x-1,map)))
                 return true;
             return false;
         }
+    }
+
+    private boolean isBo(int y, int x, int[][] map){
+        return (map[y][x] ^ (BO+1)) < 3;
+    }
+    private boolean isPillar(int y, int x, int[][] map){
+        return (map[y][x] ^ (PILLAR+1)) < 3;
     }
     private boolean outOfBound(int y, int x, int n){
         return y<0||y>n||x<0||x>n;
