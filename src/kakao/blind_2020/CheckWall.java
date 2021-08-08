47483c9ddb295ac5b3f491be85166a49bbd6b13e
brandpark <pak2005@hanmail.net>
2021-08-08 23:50:25 +0900

    카카오 2020 블라인드 채용_외벽 점검(완료)

--- src/kakao/blind_2020/CheckWall.java
+++ src/kakao/blind_2020/CheckWall.java
@@ -7,16 +7,86 @@ package kakao.blind_2020;
  * link : https://programmers.co.kr/learn/courses/30/lessons/60062
  * */
 public class CheckWall {
-    // 외벽의 길이 n, 1<= n <= 200
-    // 취약지점 위치 배열 weak, 1<= weak.length <= 15,
-    // 취약지점 k는 0<= k <= n-1, k는 시계방향으로의 거리
-    // 각 친구가 1시간 동안 이동가능한 거리 배열 dist, 1<= dist.length <= 100
-    // 취약 지점을 점검하기 위해 보내야 하는 친구 수의 최소값 return
-    // 취약 지점을 전부 점검할 수 없는 경우에는 -1리턴
-    // solution : 출발 지점부터 시계 또는 반시계 방향으로 외벽을 따라 이동하여 취약지점을 전부 점검하기 위해 보내야 하는 친구의 최소 수를 구해야한다.
-    //              출발 지점은 마음대로이며, 자신이 가는 길의 취약점은 모두 점검 가능하다.
+    // 1 <= n <= 200
+    // 1 <= weak.length <= 15, 오름차순, 0이상 n-1 이하의 정수
+    // 1 <= dist.length <= 8, 1이상 100이하 자연수
+    // 모두 투입해도 불가능하면 -1 리턴
+    // return : 모두 점검 가능한 최소 인원 수
+
+    private int n, answer;
+    private int[] weak, dist, weakSpread;
+    private boolean finish;
+
     public int solution(int n, int[] weak, int[] dist) {
-        int answer = 0;
+        this.n = n;
+        this.answer = -1;
+        this.weak = weak;
+        this.dist = dist;
+        this.weakSpread = new int[2 * n];
+
+        initSpreadWeak();
+
+        for (int i = 1; i <= dist.length; i++) {
+            permutation(i, 0, new boolean[dist.length], new int[i]);
+        }
+
         return answer;
+    }
+
+    private void permutation(int limit, int depth, boolean[] visit, int[] dist2) {
+        if (finish) {
+            return;
+        }
+        if (limit == depth) {
+            check(dist2);
+            return;
+        }
+        for (int i = 0; i < dist.length; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                dist2[depth] = dist[i];
+                permutation(limit, depth + 1, visit, dist2);
+                visit[i] = false;
+            }
+        }
+    }
+
+    private void check(int[] dist2) {
+        for (int startIdx = 0; startIdx < weak.length; startIdx++) {
+            int endIdx = startIdx + weak.length - 1;
+            if (endIdx >= weakSpread.length) {
+                answer = -1;
+                finish = true;
+                return;
+            }
+
+            int cur = weak[startIdx];
+            for (int d : dist2) {
+                cur += d;
+                if (cur >= weakSpread[endIdx]) {
+                    answer = dist2.length;
+                    finish = true;
+                    return;
+                }
+                for (int t = startIdx; t <= endIdx; t++) {
+                    if(weakSpread[t] > cur){
+                        cur = weakSpread[t];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void initSpreadWeak() {
+        for (int i = 0; i < weakSpread.length; i++) {
+            int idx = i;
+            if (i >= weak.length) {
+                idx %= weak.length;
+                weakSpread[i] = weak[idx] + n;
+            } else {
+                weakSpread[i] = weak[idx];
+            }
+        }
     }
 }
